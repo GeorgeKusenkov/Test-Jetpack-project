d3ed6ee044bd8111e97f794ff2d7c59d43371b36
GeorgeKusenkov <g-jazzman@yandex.ru>
2022-10-07 16:50:57 +0500

    modifier

--- app/src/main/java/com/example/jetpack/MainActivity.kt
+++ app/src/main/java/com/example/jetpack/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.jetpack
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,9 +27,27 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column {
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
                 ListItem("Dude 1", "Android Dev")
                 ListItem("Dude 2", "Android Dev")
+                ListItem("Dude 3", "Android Dev")
+                ListItem("Dude 3", "Android Dev")
+                ListItem("Dude 3", "Android Dev")
+                ListItem("Dude 3", "Android Dev")
+                ListItem("Dude 3", "Android Dev")
+                ListItem("Dude 3", "Android Dev")
+                ListItem("Dude 3", "Android Dev")
+                ListItem("Dude 3", "Android Dev")
+                ListItem("Dude 3", "Android Dev")
+                ListItem("Dude 3", "Android Dev")
+                ListItem("Dude 3", "Android Dev")
+                ListItem("Dude 3", "Android Dev")
+                ListItem("Dude 3", "Android Dev")
+                ListItem("Dude 3", "Android Dev")
+                ListItem("Dude 3", "Android Dev")
+                ListItem("Dude 3", "Android Dev")
+                ListItem("Dude 3", "Android Dev")
+                ListItem("Dude 3", "Android Dev")
                 ListItem("Dude 3", "Android Dev")
             }
 
@@ -40,7 +61,12 @@ private fun ListItem(name: String, profession: String) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(10.dp),
+            .padding(10.dp)
+            .pointerInput(Unit) {
+                detectTapGestures{
+                    Log.d("test_msg", "Long press: $it")
+                }
+            },
         shape = RoundedCornerShape(15.dp),
         elevation = 5.dp
         ) {
